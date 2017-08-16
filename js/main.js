df7dc39d89fe12e4b378116eede89794327c0a41
Asil Cetin <asilcetin@users.noreply.github.com>
2017-08-16 15:06:58 +0000

    closes #6

--- js/main.js
+++ js/main.js
@@ -1,53 +1,48 @@
-jQuery(function ($) {
-"use strict"; 
-/* You can safely use $ in this code block to reference jQuery */
+jQuery(function($) {
+    "use strict";
+    /* You can safely use $ in this code block to reference jQuery */
 
-$(document).scroll(function() {
-  if ($(document).scrollTop() >= 53) {
-    $('.info-menu-wrap').addClass('fixed-sidebar');
-  } else {
-    $('.info-menu-wrap').removeClass('fixed-sidebar');
-  }
-});
+    // Info menu scroll behaviour
+    $(document).scroll(function() {
+        if ($(document).scrollTop() >= 53) {
+            $('.info-menu-wrap').addClass('fixed-sidebar');
+        } else {
+            $('.info-menu-wrap').removeClass('fixed-sidebar');
+        }
+    });
 
-$(document).ready(function(){
-	
-  if ($(document).scrollTop() >= 53) {
-    $('.info-menu-wrap').addClass('fixed-sidebar');
-  } else {
-    $('.info-menu-wrap').removeClass('fixed-sidebar');
-  }	
-	
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+    $(document).ready(function() {
+		// Info menu new page load behaviour
+        if ($(document).scrollTop() >= 53) {
+            $('.info-menu-wrap').addClass('fixed-sidebar');
+        } else {
+            $('.info-menu-wrap').removeClass('fixed-sidebar');
+        }
+        // Add smooth scrolling to inside-page links
+        $("a").on('click', function(event) {
+            // Make sure this.hash has a value before overriding default behavior
+            if (this.hash !== "") {
+                // Store hash
+                var hash = this.hash;
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top - 160
+                }, 800, function() {
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    //window.location.hash = hash;
+                });
+            } // End if
+        });
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-      // Store hash
-      var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top - 160
-      }, 800, function(){   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-  
-  
-	// Make sure this.hash has a value before overriding default behavior
-	if (window.location.hash !== "") {
-	  // Store hash
-	  var hash = window.location.hash;
-	  $('html, body').animate({
-	    scrollTop: $(hash).offset().top - 90
-	  }, 800, function(){   
-	  });
-	} // End if  
-  
-});
+        // Add smooth scrolling to outside-page links
+        if (window.location.hash !== "") {
+            // Store hash
+            var hash = window.location.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - 90
+            }, 800, function() {});
+        } // End if  
 
-/* You can safely use $ in this code block to reference jQuery */
+    });
+
+    /* You can safely use $ in this code block to reference jQuery */
 });
