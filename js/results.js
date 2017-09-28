3c02472579e022cd9c662cc560c4f4ab386afebb
Asil Çetin <asilcetin@users.noreply.github.com>
2017-09-28 16:36:59 +0000

    minor changes

--- js/results.js
+++ js/results.js
@@ -36,12 +36,9 @@ $('.res-act-button-expertview').click(function() {
 	}
 }); 
 
-
-
-
 $("#edit-date-start-date")
   .datepicker({
-    dateFormat: "yy/mm/dd",
+    dateFormat: "dd/mm/yy",
     changeYear: true,
     showOn: "button",
     constrainInput: true
@@ -52,7 +49,7 @@ $("#edit-date-start-date")
 
 $("#edit-date-end-date")
   .datepicker({
-    dateFormat: "yy/mm/dd",
+    dateFormat: "dd/mm/yy",
     changeYear: true,
     showOn: "button",
     constrainInput: true
@@ -61,41 +58,33 @@ $("#edit-date-end-date")
   .addClass("date-filter-btn")
   .html('<i class="material-icons">&#xE878;</i>')
 
-
 $("input[type=text].date-filter").keyup(function (e) {
-        var textSoFar = $(this).val();
-        if (e.keyCode != 191) {
-            if (e.keyCode != 8) {
-                if (textSoFar.length == 2 || textSoFar.length == 5) {
-                    $(this).val(textSoFar + "/");
-                }
-                    //to handle copy & paste of 8 digit
-                else if (e.keyCode == 86 && textSoFar.length == 8) {
-                    $(this).val(textSoFar.substr(0, 2) + "/" + textSoFar.substr(2, 2) + "/" + textSoFar.substr(4, 4));
-                }
+    var textSoFar = $(this).val();
+    if (e.keyCode != 191) {
+        if (e.keyCode != 8) {
+            if (textSoFar.length == 2 || textSoFar.length == 5) {
+                $(this).val(textSoFar + "/");
             }
-            else {
-                //backspace would skip the slashes and just remove the numbers
-                if (textSoFar.length == 5) {
-                    $(this).val(textSoFar.substring(0, 4));
-                }
-                else if (textSoFar.length == 2) {
-                    $(this).val(textSoFar.substring(0, 1));
-                }
+                //to handle copy & paste of 8 digit
+            else if (e.keyCode == 86 && textSoFar.length == 8) {
+                $(this).val(textSoFar.substr(0, 2) + "/" + textSoFar.substr(2, 2) + "/" + textSoFar.substr(4, 4));
             }
         }
         else {
-            //remove slashes to avoid 12//01/2014
-            $(this).val(textSoFar.substring(0, textSoFar.length - 1));
+            //backspace would skip the slashes and just remove the numbers
+            if (textSoFar.length == 5) {
+                $(this).val(textSoFar.substring(0, 4));
+            }
+            else if (textSoFar.length == 2) {
+                $(this).val(textSoFar.substring(0, 1));
+            }
         }
-    });
-
-
-
-
-
-
-
+    }
+    else {
+        //remove slashes to avoid 12//01/2014
+        $(this).val(textSoFar.substring(0, textSoFar.length - 1));
+    }
+});
 
 /* You can safely use $ in this code block to reference jQuery */
 });
