e448b885d449a81244a59949810ae0b4f1054e11
Asil Çetin <asilcetin@users.noreply.github.com>
2017-09-28 16:05:25 +0000

    date of publication filter implementation

--- js/results.js
+++ js/results.js
@@ -38,5 +38,64 @@ $('.res-act-button-expertview').click(function() {
 
 
 
+
+$("#edit-date-start-date")
+  .datepicker({
+    dateFormat: "yy/mm/dd",
+    changeYear: true,
+    showOn: "button",
+    constrainInput: true
+  })
+  .next("button").button()
+  .addClass("date-filter-btn")
+  .html('<i class="material-icons">&#xE8DF;</i>')
+
+$("#edit-date-end-date")
+  .datepicker({
+    dateFormat: "yy/mm/dd",
+    changeYear: true,
+    showOn: "button",
+    constrainInput: true
+  })
+  .next("button").button()
+  .addClass("date-filter-btn")
+  .html('<i class="material-icons">&#xE878;</i>')
+
+
+$("input[type=text].date-filter").keyup(function (e) {
+        var textSoFar = $(this).val();
+        if (e.keyCode != 191) {
+            if (e.keyCode != 8) {
+                if (textSoFar.length == 2 || textSoFar.length == 5) {
+                    $(this).val(textSoFar + "/");
+                }
+                    //to handle copy & paste of 8 digit
+                else if (e.keyCode == 86 && textSoFar.length == 8) {
+                    $(this).val(textSoFar.substr(0, 2) + "/" + textSoFar.substr(2, 2) + "/" + textSoFar.substr(4, 4));
+                }
+            }
+            else {
+                //backspace would skip the slashes and just remove the numbers
+                if (textSoFar.length == 5) {
+                    $(this).val(textSoFar.substring(0, 4));
+                }
+                else if (textSoFar.length == 2) {
+                    $(this).val(textSoFar.substring(0, 1));
+                }
+            }
+        }
+        else {
+            //remove slashes to avoid 12//01/2014
+            $(this).val(textSoFar.substring(0, textSoFar.length - 1));
+        }
+    });
+
+
+
+
+
+
+
+
 /* You can safely use $ in this code block to reference jQuery */
 });
