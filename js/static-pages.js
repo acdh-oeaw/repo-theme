45d14e27c7023b08c3cfe06d9cc23d3c2182a612
Norbert Czirjak <Norbert.Czirjak@oeaw.ac.at>
2023-08-04 10:27:20 +0200

    static page urls added

--- js/static-pages.js
+++ js/static-pages.js
@@ -1,39 +1,37 @@
 jQuery(function ($) {
     "use strict";
     var currentlang = jQuery('html').attr('lang');
-    var archeApiUrl = "/browser/api/static-pages/";
     $(document).ready(function () {
         depositionProcess();
     });
 
     function fetchGithubPage(githubUrl) {
         $.get(githubUrl, function (response) {
-                $("article.contextual-region").html(response);
-            }).fail(function (jqXHR, textStatus, errorThrown) {
-                // Error callback function
-                console.log("Error during github page fetch:", textStatus, errorThrown);
-                // Handle the error response here
-            });
+            $("article.contextual-region").html(response);
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            // Error callback function
+            console.log("Error during github page fetch:", textStatus, errorThrown);
+            // Handle the error response here
+        });
     }
 
 
+    /**
+     * this function is fetching the static pages from the gihub repo https://github.com/acdh-oeaw/arche-static-text
+     * @returns {undefined}
+     */
     function depositionProcess() {
-        if (window.location.href.indexOf("browser/deposition-process") >= 0) {
-            var lastPart = window.location.href.substring(window.location.href.lastIndexOf("/") + 1);
-            const githubUrl = "https://raw.githubusercontent.com/acdh-oeaw/arche-static-text/master/"+currentlang+'/'+lastPart+'.html';
-            fetchGithubPage(githubUrl);
-        }
-        
-        if (window.location.href.indexOf("browser/formats-filenames-and-metadata") >= 0) {
-            var lastPart = window.location.href.substring(window.location.href.lastIndexOf("/") + 1);
-            const githubUrl = "https://raw.githubusercontent.com/acdh-oeaw/arche-static-text/master/"+currentlang+'/'+lastPart+'.html';
-            fetchGithubPage(githubUrl);
-        }
-        
-        if (window.location.href.indexOf("browser/technical-setup") >= 0) {
-            var lastPart = window.location.href.substring(window.location.href.lastIndexOf("/") + 1);
-            const githubUrl = "https://raw.githubusercontent.com/acdh-oeaw/arche-static-text/master/"+currentlang+'/'+lastPart+'.html';
-            fetchGithubPage(githubUrl);
-        }
+
+        var urls = ['about-service', 'api-access', 'collection-policy', 'deposition-agreement',
+            'deposition-process', 'faq', 'formats-filenames-and-metadata', 'further-guidance',
+            'preservation-policy', 'privacy', 'technical-setup', 'terms-of-use'];
+
+        $.each(urls, function (index, element) {
+            if (window.location.href.indexOf("browser/" + element) >= 0) {
+                var lastPart = window.location.href.substring(window.location.href.lastIndexOf("/") + 1);
+                const githubUrl = "https://raw.githubusercontent.com/acdh-oeaw/arche-static-text/master/" + currentlang + '/' + lastPart + '.html';
+                fetchGithubPage(githubUrl);
+            }
+        });
     }
 });
